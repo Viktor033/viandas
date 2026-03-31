676bd34661863490345f7c8a5f91ddbc152e9111
Viktor33 <vk.developer.web@gmail.com>
2026-03-31 11:21:03 -0300

    AGREGANDO PLAN V0.7

--- src/main/java/com/manoplas/viandas/controller/PedidoController.java
+++ src/main/java/com/manoplas/viandas/controller/PedidoController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v2/pedidos")
+@RequestMapping({"/api/v2/pedidos", "/api/pedidos"})
 public class PedidoController {
 
     @Autowired
